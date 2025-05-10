fbbf3a581c0f55e048929d8cfe7b5e534855e23c
Damian <josepwnz@gmail.com>
2025-05-10 21:01:09 +0200

    bug ui fix

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex min-h-screen">
-    <Sidebar class="w-64 bg-gray-100 p-4 shadow-md" />
+    <Sidebar />
     <main class="flex-1 p-6 bg-white">
       <router-view />
     </main>
