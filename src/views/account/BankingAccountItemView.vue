41bedcc0177eabdb38dec28e054a12f1443db811
Damian <josepwnz@gmail.com>
2025-05-22 23:40:47 +0200

    Renombrado de componentes

---
+++ src/views/account/BankingAccountItemView.vue
@@ -0,0 +1,182 @@
+<script setup lang="ts">
+import { MessageType } from "@/types/Message";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { useAccountStore } from "@/stores/account";
+import { useAuthStore } from "@/stores/auth";
+import { SquarePen, ChevronRight, ChevronLeft } from "lucide-vue-next";
+import BankingAccount from "@/views/account/components/BankingAccount.vue";
+import RequestBankingCardModal from "@/views/account/components/RequestBankingCardModal.vue";
+// message to show
+const messageAlert = ref({
+  message: "",
+  type: MessageType.ERROR,
+  timeout: 12,
+  visible: false,
+});
+
+const route = useRoute();
+const accountStore = useAccountStore();
+const authStore = useAuthStore();
+const account = ref();
+
+const modals = {
+  bankingCard: {
+    visible: ref(false),
+  },
+};
+
+function transferTo() {
+  // ...
+}
+
+function previousPage() {
+  // ...
+}
+function nextPage() {
+  // ...
+}
+
+async function requestCard(cardType: string) {
+  // hide modal
+  modals.bankingCard.visible.value = false;
+
+  // account id from url
+  const accountId = parseInt(route.params.id[0]);
+
+  // token
+  const token = authStore.token;
+
+  // petition
+  const response = await accountStore.requestBankingCard(
+    token,
+    accountId.toString(),
+    cardType
+  );
+
+  if (response.status !== 201 && response.data.error) {
+    messageAlert.value.message = response.data.message;
+    return;
+  }
+
+  accountStore.addCard(accountId, response.data);
+}
+
+onMounted(() => {
+  const accountId = parseInt(route.params.id as string, 10);
+  // account.value = accountStore.getBankingAccount.find(
+  //   (a) => a.id === accountId
+  // );
+  account.value = accountStore.getBankingAccount(accountId);
+  if (!account.value) {
+    messageAlert.value.message = "No se encontró la cuenta bancaria.";
+  }
+});
+</script>
+<template>
+  <div v-if="account">
+    <MessageAlert
+      v-if="messageAlert.visible"
+      class="mb-4"
+      :message="messageAlert.message"
+      :timeout="messageAlert.timeout"
+      :type="messageAlert.type"
+      @close="messageAlert.visible = false"
+    />
+
+    <div class="p-4 rounded bg-blue-50 shadow">
+      <div>
+        <h1 class="flex items-center text-2xl font-bold gap-1">
+          <span>Banking account ({{ account.alias }})</span>
+          <SquarePen />
+          <span
+            class="text-xs rounded-full px-1"
+            :class="{
+              'text-orange-100 bg-orange-500':
+                account?.accountStatus === 'CLOSED',
+              'text-red-100 bg-red-500': account?.accountStatus === 'SUSPENDED',
+              'text-green-100 bg-green-500': account?.accountStatus === 'OPEN',
+            }"
+            >{{ account?.accountStatus }}
+          </span>
+          <div class="flex flex-wrap gap-1">
+            <button
+              @click="transferTo"
+              class="btn-small btn-blue w-full sm:w-auto"
+            >
+              TRANSFER TO
+            </button>
+            <button
+              @click="transferTo"
+              class="btn-small btn-blue w-full sm:w-auto"
+            >
+              REQUEST CARD
+            </button>
+            <button
+              @click="transferTo"
+              class="btn-small btn-blue w-full sm:w-auto"
+            >
+              SET ALIAS
+            </button>
+            <button
+              @click="transferTo"
+              class="btn-small btn-blue w-full sm:w-auto"
+            >
+              {{ account?.accountStatus === "OPEN" ? "CLOSE" : "OPEN" }}
+              ACCOUNT
+            </button>
+          </div>
+        </h1>
+      </div>
+
+      <div class="">
+        <BankingAccount v-if="account" :account="account" />
+      </div>
+
+      <div class="mt-6 p-4 bg-white rounded-xl shadow-md w-full mx-auto">
+        <h3 class="text-lg font-semibold text-gray-800 border-b pb-2 mb-4">
+          TRANSACCIONES
+        </h3>
+        <ul class="space-y-2">
+          <li
+            v-for="(transaction, index) in account?.bankingAccountTransactions"
+            :key="index"
+            class="flex justify-between items-center bg-gray-50 hover:bg-gray-100 p-3 rounded-md"
+          >
+            <span class="text-sm text-gray-700">{{
+              transaction.description
+            }}</span>
+            <span
+              class="text-sm font-medium"
+              :class="
+                transaction.amount > 0 ? 'text-green-600' : 'text-red-600'
+              "
+            >
+              {{ transaction.amount > 0 ? "+" : "" }}{{ transaction.amount }}€
+            </span>
+          </li>
+        </ul>
+        <div
+          class="flex justify-end items-center mt-4 p-0 text-white bg-blue-500 rounded-lg"
+        >
+          <button class="btn cursor-pointer" @click="previousPage">
+            <ChevronLeft />
+          </button>
+          <span> 1/2 </span>
+          <button class="btn cursor-pointer" @click="nextPage">
+            <ChevronRight />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-else>Loading account</div>
+
+  <!-- Modal -->
+  <RequestBankingCardModal
+    v-if="modals.bankingCard.visible.value"
+    :visible="modals.bankingCard.visible.value"
+    @submit="requestCard"
+    @close="modals.bankingCard.visible.value = false"
+  />
+</template>
---
+++ src/views/account/BankingAccountListView.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import BankingAccount from "@/views/account/components/BankingAccount.vue";
+import { onMounted, ref } from "vue";
+import { useAccountStore } from "@/stores/account";
+import { useAuthStore } from "@/stores/auth";
+import { MessageType } from "@/types/Message";
+import OpenAccountModal from "@/views/account/components/OpenAccountModal.vue";
+
+// message to show
+const messageAlert = ref({
+  message: "",
+  type: MessageType.ERROR,
+  timeout: 12,
+  visible: false,
+});
+
+const accountStore = useAccountStore();
+const authStore = useAuthStore();
+
+const showModal = ref(false);
+
+// function to handle the submission of the open account modal
+function submitOpenAccount(data: { type: string; currency: string }) {
+  showModal.value = false;
+  accountStore.openBankingAccount(data.type, data.currency, authStore.token);
+}
+
+onMounted(() => {
+  // accountStore.initialize();
+});
+</script>
+<template>
+  <div>
+    <MessageAlert
+      v-if="messageAlert.visible"
+      class="mb-4"
+      :message="messageAlert.message"
+      :timeout="messageAlert.timeout"
+      :type="messageAlert.type"
+      @close="messageAlert.visible = false"
+    />
+    <!-- Botón para abrir una nueva cuenta -->
+
+    <div class="flex justify-end rounded gap-1">
+      <button type="button" @click="showModal = true" class="btn btn-blue">
+        Open account
+      </button>
+    </div>
+
+    <!-- Lista de cuentas -->
+    <div v-for="account in accountStore.getBankingAccounts" :key="account.id">
+      <router-link :to="`/account/${account.id}`">
+        <BankingAccount :account="account" />
+      </router-link>
+    </div>
+  </div>
+  <!-- Modal -->
+  <OpenAccountModal
+    v-if="showModal"
+    :visible="showModal"
+    @submit="submitOpenAccount"
+    @close="showModal = false"
+  />
+</template>
