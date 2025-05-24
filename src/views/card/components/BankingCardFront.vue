c058acab37b7a58bc0af49c20923e953a1f64479
Damian <josepwnz@gmail.com>
2025-05-24 22:26:22 +0200

    NFC icon change

--- src/views/card/components/BankingCardFront.vue
+++ src/views/card/components/BankingCardFront.vue
@@ -38,50 +38,23 @@ function formatNumber(number: string): string {
     <line x1="40" y1="60" x2="40" y2="95" stroke="#eab308" stroke-width="2" />
     <line x1="50" y1="60" x2="50" y2="95" stroke="#eab308" stroke-width="2" />
 
-    <!-- NFC -->
     <g
-      transform="translate(345, 100)"
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      transform="translate(330, 90) scale(2)"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#FFFFFF"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-nfc-icon lucide-nfc"
     >
-      <!-- Onda 1 -->
-      <path
-        d="M9 11C10.5 12.5 11.5 14.5 11.5 16.5C11.5 18.5 10.5 20.5 9 22"
-        stroke="#FFFFFF"
-        stroke-width="2"
-        stroke-linecap="round"
-      />
-      <!-- Onda 2 -->
-      <path
-        d="M12.5 8.5C14.8 10.4 16.2 13.2 16.2 16.5C16.2 19.8 14.8 22.6 12.5 24.5"
-        stroke="#FFFFFF"
-        stroke-width="2"
-        stroke-linecap="round"
-      />
-      <!-- Onda 3 -->
-      <path
-        d="M16 6C19 8.5 20.7 12 20.7 16.5C20.7 21 19 24.5 16 27"
-        stroke="#FFFFFF"
-        stroke-width="2"
-        stroke-linecap="round"
-      />
-      <!-- Onda 4 -->
-      <path
-        d="M19.5 3.5C22.8 6.4 24.5 11 24.5 16.5C24.5 22 22.8 26.6 19.5 29.5"
-        stroke="#FFFFFF"
-        stroke-width="2"
-        stroke-linecap="round"
-      />
-      <!-- Onda 5 -->
-      <path
-        d="M23 1C27 4.6 28.5 10 28.5 16.5C28.5 23 27 28.4 23 32"
-        stroke="#FFFFFF"
-        stroke-width="2"
-        stroke-linecap="round"
-      />
+      <path d="M6 8.32a7.43 7.43 0 0 1 0 7.36" />
+      <path d="M9.46 6.21a11.76 11.76 0 0 1 0 11.58" />
+      <path d="M12.91 4.1a15.91 15.91 0 0 1 .01 15.8" />
+      <path d="M16.37 2a20.16 20.16 0 0 1 0 20" />
     </g>
 
     <!-- Nombre del banco -->
